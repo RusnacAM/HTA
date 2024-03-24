1f1826e18932f9958592b984c035d686ca284051
Rusnac Ana-Maria <rusnacanna.maria@gmail.com>
2024-03-24 21:15:27 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold flex h-screen justify-center items-center">
+      <h1> PROJECT INIT</h1>
+    </div>
+  );
+}
+
+export default App;
