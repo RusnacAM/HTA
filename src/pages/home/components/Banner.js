6bbf9c81871ce63b9d4422683b5ec8f9235c2fe2
Rusnac Ana-Maria <rusnacanna.maria@gmail.com>
2024-04-30 19:17:28 +0300

    route redirects

--- src/pages/home/components/Banner.js
+++ src/pages/home/components/Banner.js
@@ -1,11 +1,18 @@
 import Button from "../../../atoms/buttons";
+import {useNavigate} from "react-router-dom";
 
 const Banner = () => {
+    let navigate = useNavigate()
+    const routeChange = (path) => {
+        navigate(path)
+    }
+
     return (
-        <section className="h-[85vh] rounded-b-[3.0rem] bg-gray-300 bg-no-repeat bg-cover w-screen bg-[url('http://i.imgur.com/oke2LuI.png')]" >
+        <section className="h-[90vh] rounded-b-xl bg-gray-300 bg-no-repeat bg-cover w-screen bg-[url('http://i.imgur.com/oke2LuI.png')]" >
             <div className="w-full h-full flex flex-col justify-center items-center gap-y-5">
                 <h1 className="text-4xl text-white text-center font-semibold tracking-wide">Turning Awareness into Action: <br /> Join the Fight Against Human Trafficking</h1>
                 <Button
+                    onClick={() => routeChange("/share")}
                     intent={"primary"}
                     size={"large"}
                     corner={"rectangle"}
--- src/pages/info/components/ContactSection.js
+++ src/pages/info/components/ContactSection.js
@@ -1,6 +1,12 @@
 import Button from "../../../atoms/buttons";
+import {useNavigate} from "react-router-dom";
 
 const ContactSection = () => {
+    let navigate = useNavigate()
+    const routeChange = (path) => {
+        navigate(path)
+    }
+
     return (
         <section className="h-[60vh] w-full bg-black bg-gradient-to-t from-blue-900 via-black to-black text-white w-screen" >
             <div className="w-full h-full flex flex-col justify-center items-center gap-y-5 px-20">
@@ -8,6 +14,7 @@ const ContactSection = () => {
                 <br />
                 <p className="indent-10 w-[70%]">If you or someone you know is currently experiencing or at risk of human trafficking, it's crucial to reach out for help. Your safety and well-being are paramount, and there are organizations and support services available to assist you in exiting exploitative situations and accessing the assistance you need. Whether you're a victim of forced labor, sexual exploitation, or any form of trafficking, know that you are not alone and help is available. <br /> By reaching out to trusted individuals, hotlines, or support organizations, you can access resources such as shelter, legal assistance, counseling, and rehabilitation services. Your courage in seeking help can make all the difference in breaking free from trafficking and reclaiming your freedom and dignity. Don't hesitate to reach out – together, we can work towards ending human trafficking and ensuring a safer, more just world for all.</p>
                 <Button
+                    onClick={() => routeChange("/share")}
                     intent={"primary"}
                     size={"large"}
                     corner={"rectangle"}
